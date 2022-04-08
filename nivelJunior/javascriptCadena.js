c56acde820e85a41d842e07058992f8aee858d5f
unknown <david12.29@hotmail.com>
2022-04-08 11:54:28 -0500

    Empece a mirar cadenas 2

--- nivelJunior/javascriptCadena.js
+++ nivelJunior/javascriptCadena.js
@@ -9,7 +9,20 @@ let resultado2=cadena.startsWith(cadena2);
 // Me devuelve un indece de la posicion de la palabra buscada si da -1 es porque no lo encontro
 let resultado3=cadena.indexOf("de");
 
+// devuelve la posicion (indice) 
+let resultado4=cadena.lastIndexOf("de");
+
+// Rellena la cadena para cumplir el numero de espacio necesarios que uno pide
+let resultado5=cadena.padStart(26,"de");
+
+// Hace lo mismo que padStart solo que al final
+let resultado6=cadena.padEnd(26,"de");
+
+
 
 document.write(resultado+"<br>");
 document.write(resultado2+"<br>");
 document.write(resultado3+"<br>");
+document.write(resultado4+"<br>");
+document.write(resultado5+"<br>");
+document.write(resultado6+"<br>");
