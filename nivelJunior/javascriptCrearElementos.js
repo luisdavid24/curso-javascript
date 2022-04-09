0fdeb79d6226c77d5d277dbeb3787e7111d86a11
Luis david <david12.29@hotmail.com>
2022-04-09 18:36:02 -0500

    Trabajando con childs

--- nivelJunior/javascriptCrearElementos.js
+++ nivelJunior/javascriptCrearElementos.js
@@ -14,10 +14,30 @@ const fragmento=document.createDocumentFragment();
 
 // document.write(item);
 
-for(let i=0;i<20;i++){
+for(let i=0;i<2;i++){
     item=document.createElement("LI");
     item.innerHTML="Este es un item de la lista";
     fragmento.appendChild(item);
 
 }
 contenedor.appendChild(fragmento);
+
+
+const contenedor2=document.querySelector(".contenedor2");
+
+// cuando se usa firstChild los espacio se toman como elementos  
+// Cuando usamos firstElementChild nos trae el primer elemmento encontrado
+// Cuando usamos childNodes nos da un node list con todos los hijos no un arreglo
+// Conn children solo devuelve las etiquetas sin el contenido
+const primerHijo=contenedor.firstElementChild;
+
+console.log(primerHijo);
+
+const contenedor3=document.querySelector(".contenedor3");
+const parrafo=document.createElement("P").innerHTML="Parafo";
+const h2Nuevo=document.createElement("H2");
+h2Nuevo.innerHTML="Titulo";
+
+h2Antiguo=document.querySelector(".h2Viejo");
+
+contenedor3.replaceChild(h2Nuevo,h2Antiguo);
