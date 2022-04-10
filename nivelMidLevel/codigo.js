e0d25d363f9b97280a453ae3b3cd5777fa7f867d
Luis david <david12.29@hotmail.com>
2022-04-10 18:02:49 -0500

    usando metodos del objeto windows

---
+++ nivelMidLevel/codigo.js
@@ -0,0 +1,13 @@
+let youtubeURL="https://youtube.com";
+
+let ventana= window.open(youtubeURL);
+
+ventana.close();
+
+// prompt("introduce tu dato, siempre lo recibe y lo vuelve un string");
+confirm("Pregunta y nos da dos opciones si o no, devuelve un booleano");
+
+
+// window.screen; es un objeto que trabaja con la pantalla
+// print(); con el print permite imprimir
+// closed es para parar la carga de una ventana  
