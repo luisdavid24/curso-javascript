a4776faed59220a8ecc40fb9c6f986477624ee8f
Luis david <david12.29@hotmail.com>
2022-04-09 21:44:27 -0500

    se termino el nivel junior

---
+++ nivelJunior/codigo.js
@@ -0,0 +1,36 @@
+const contenedor=document.querySelector(".flex-container");
+
+
+function crearLlave(nombre,modelo,precio){
+    img="<img  class='llave-img'src='llave.png'>";
+    nombre=`<h2>${nombre}</h2>`;
+    modelo=`<h3>${modelo}</h3>`;
+    precio=`<p>Precio: <b>$${precio}</b><p/>`;
+    return [img,nombre,modelo,precio];
+}
+
+const llave=crearLlave("Llave1","Modelo X","33");
+
+// contenedor.innerHTML=llave[0]+llave[1]+llave[2];
+
+
+const changeHiden=(number)=>{
+    document.querySelector(".key-data").value=number;
+}
+
+let documentFragment=document.createDocumentFragment();
+
+for(let i=0;i<20;i++){
+    let modeloRandom=Math.round(Math.random()*10000);
+    let precioRandom=Math.round(Math.random()*10+30);
+
+    let llave=crearLlave(`Llave ${i}`,`modelo ${modeloRandom}`,precioRandom);
+    let div=document.createElement("DIV");
+    div.addEventListener("click",()=>{ (modeloRandom)})
+    div.tabIndex=i;
+    div.classList.add(`item-${i}`,"flex-item");
+    div.innerHTML=llave[0]+llave[1]+llave[2]+llave[3];
+    documentFragment.appendChild(div);
+}
+
+contenedor.appendChild(documentFragment);
