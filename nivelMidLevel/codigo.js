1c214aeb21b6b95b1b9c1c81092b55e69d8b5bc6
Luis david <david12.29@hotmail.com>
2022-04-10 21:27:00 -0500

    estoy en el curso medio

--- nivelMidLevel/codigo.js
+++ nivelMidLevel/codigo.js
@@ -1,13 +1,24 @@
-let youtubeURL="https://youtube.com";
-
-let ventana= window.open(youtubeURL);
-
-ventana.close();
+// let youtubeURL="https://youtube.com";
+// let ventana= window.open(youtubeURL);
+// ventana.close();
 
 // prompt("introduce tu dato, siempre lo recibe y lo vuelve un string");
-confirm("Pregunta y nos da dos opciones si o no, devuelve un booleano");
+
+// confirm("Pregunta y nos da dos opciones si o no, devuelve un booleano");
 
 
 // window.screen; es un objeto que trabaja con la pantalla
+// window.screenLeft; dice la distancia de la ventana con la parte izquierda de la pantalla
+
+
 // print(); con el print permite imprimir
 // closed es para parar la carga de una ventana  
+
+// scrollX; scroll en x 
+// scrollY; scroll en y
+
+// nos dice la locasion
+const herf=window.location.href;
+
+console.log(herf);
+console.log(window.location.pathname);
