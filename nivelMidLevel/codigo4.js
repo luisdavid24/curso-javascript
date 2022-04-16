9dd7664773dc0cafb0490577f9a1cc53402f41d6
Luis david <david12.29@hotmail.com>
2022-04-15 20:44:31 -0500

    estoy trabajando con temporizadores

---
+++ nivelMidLevel/codigo4.js
@@ -0,0 +1,18 @@
+
+const saludar=()=>{
+    document.write("hola, te gusta la piña")
+};
+setTimeout(saludar,2000);
+
+
+const temporizador=setTimeout(()=>{
+    document.write("Te gusta la carne")
+},3000);
+
+clearInterval(temporizador);//Aqui no se va a ejecutar el temporizador
+
+const intervalo=setInterval(()=>{
+    document.write("Te gusta la carne invervalo")
+},3000);
+
+clearInterval(intervalo);
